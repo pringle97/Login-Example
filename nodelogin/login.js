158a21662895690bc8f9d7bd09fa560f5b93e534
pringle97 <malia.pringle@gmail.com>
2022-03-18 23:49:06 -0700

    created connection to mysql and styled login page

---
+++ nodelogin/login.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql');
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'nodelogin'
+});
+
+//initializes express
+const app = express();
+
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
